69c3818bf597fb743f9e41642c5f17624a600ae8
Roshan Shrestha <shrestharoshan776@gmail.com>
2024-06-01 22:11:34 -0400

    Homepage UI and CSS updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+import './App.css';
+function App() { return (
+<div className="assignment-container">
+  <h1>CSD 4503 – DevOp Tools and Practices – 2024S</h1>
+  <p><strong>Student ID:</strong> C0901142</p>
+  <p><strong>Student Name:</strong> Roshan Shrestha</p>
+  <p><strong>Assignment #1</strong></p>
+  <br />
+  <h2>Description</h2>
+  <p>
+    To demonstrate understanding and practical skills in using Git and GitHub,
+    including initializing a repository, making commits, creating and merging
+    branches, and collaborating using push and pull operations.
+  </p>
+</div>
+); }; export default App
